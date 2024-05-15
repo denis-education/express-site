5d6a363865cfb04d70a07dba6540abeecae793e4
denis-education <denisss.education@gmail.com>
2024-05-15 23:53:45 +0700

    update commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express"
+import bodyParser from "body-parser";
 
 
 const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (reg, res) =>{
-    res.send("Hello?");
+    res.render("index.ejs");
+})
+
+app.post('/', (req, res) => {
+    const username = req.body.username;
+    res.render('hello.ejs', {username: username});
 })
 
 const port = process.env.port || 8080
-app.listen(port, () => console.log('listen port: ${port}'))
+app.listen(port, () => console.log('listen port: ' + port))
